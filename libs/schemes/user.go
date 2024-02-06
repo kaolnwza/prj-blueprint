0da7955a85c44251f9e8effd4562b988ae118f15
sinlapawit.r <sinlapawit.r@infinitaskt.com>
2024-02-06 17:26:03 +0700

    fix: refactor folder as lib

--- libs/schemes/user.go
+++ libs/schemes/user.go
@@ -1,7 +1,11 @@
 package schemes
 
-type User struct {
+type Users struct {
 	UserId    string `gorm:"column:id; primary_key; type:varchar(36); default:gen_random_uuid()"`
 	Firstname string `gorm:"column:firstname; type:varchar(255); not null"`
 	Lastname  string `gorm:"column:lastname; type:varchar(255); not null"`
+}
+
+func (Users) TableName() string {
+	return "users"
 }
--- libs/schemes/wallets.go
+++ libs/schemes/wallets.go
@@ -1,8 +1,12 @@
 package schemes
 
-type Wallet struct {
+type Wallets struct {
 	WalletId string  `gorm:"column:wallet_id; primary_key; type:varchar(36); default:gen_random_uuid()"`
 	UserId   string  `gorm:"column:user_id; type:varchar(36); not null; index"`
 	Amount   float64 `gorm:"column:amount; type:float; not null; default:0"`
-	User     User    `gorm:"references:UserId"`
+	User     Users   `gorm:"references:UserId"`
+}
+
+func (Wallets) TableName() string {
+	return "wallets"
 }
